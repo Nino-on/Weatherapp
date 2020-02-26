e8b75166912d6675e7eb5e1ea440f56eabcab53a
Ren <raulnino24@gmail.com>
2020-02-26 18:33:56 -0500

    showing response

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ function searchCity(){
   success: function (response) {
     console.log(response);
     console.log(response.list[0].weather[0].description);
+    console.log(response.list[0].main.humidity);
+    console.log(response.city.name);
+
+    $("#cityName").text(response.city.name);
+    $("#cityHumidity").append(response.list[0].main.humidity);
+    $("#cityTemp").append(response.list[0].main.temp);
+    $("#cityWindSpeed").append(response.list[0].wind.speed);
+ 
   }
   })
 }
